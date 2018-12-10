28363059c90123a40f794def060094de302a794c
jddelia <jddelia123@gmail.com>
2018-12-09 19:13:43 -0500

    Update

---
+++ build/precache-manifest.584e51b06161f047e059be9f5fa8e729.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72b6656c780023c1d612",
+    "url": "/reactcalc/static/js/runtime~main.72b6656c.js"
+  },
+  {
+    "revision": "51d9d32c345353d4327f",
+    "url": "/reactcalc/static/js/main.51d9d32c.chunk.js"
+  },
+  {
+    "revision": "09689009d67fed834754",
+    "url": "/reactcalc/static/js/1.09689009.chunk.js"
+  },
+  {
+    "revision": "51d9d32c345353d4327f",
+    "url": "/reactcalc/static/css/main.fdfc056b.chunk.css"
+  },
+  {
+    "revision": "e890664178ce218428bab7bebf685b60",
+    "url": "/reactcalc/index.html"
+  }
+];
